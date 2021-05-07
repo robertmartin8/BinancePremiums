65fce57c3dc2c1b89f4e3c51500266f509991edb
robertmartin8 <martin.robertandrew@gmail.com>
2021-05-07 06:54:47 +0100

    moved client into function

--- data_downloader.py
+++ data_downloader.py
@@ -3,14 +3,10 @@ from datetime import datetime as dt
 import pandas as pd
 import requests
 
-from binance.client import Client
 
-
-client = Client()
-client.ping()
-
-
-def get_coin_fut_symbols():
+def get_coin_fut_symbols(
+    client,
+):
     """
     Symbols for coin-margined delivery
     """
@@ -28,7 +24,9 @@ def get_coin_fut_symbols():
     return fut_symbols, perp_symbols
 
 
-def get_fut_symbols():
+def get_fut_symbols(
+    client,
+):
     fut_info = client.futures_exchange_info()
     fut_symbols = [
         c["symbol"]
@@ -44,7 +42,7 @@ def get_fut_symbols():
 
 
 @st.cache(ttl=310, allow_output_mutation=True)
-def get_coin_fut_premiums(timestamp=None):
+def get_coin_fut_premiums(client, timestamp=None):
     r = requests.get("https://dapi.binance.com/dapi/v1/premiumIndex")
     d = r.json()
     df = pd.DataFrame(d)
@@ -90,7 +88,7 @@ def get_coin_fut_premiums(timestamp=None):
 
 
 @st.cache(ttl=310, allow_output_mutation=True)
-def get_coin_perp_funding(timestamp=None):
+def get_coin_perp_funding(client, timestamp=None):
     fut_info = client.futures_coin_exchange_info()
     perp_symbols = [
         c["symbol"]
